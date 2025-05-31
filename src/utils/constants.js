8cc7911e2fe8f4712f15726a7038d930ba945069
Krishna <btech10725.22@bitmesra.ac.in>
2025-05-31 23:35:11 +0530

    Modified Base URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:7777";
+export const BASE_URL = "/api";
 
 export const defaultPhotoUrl = "https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
